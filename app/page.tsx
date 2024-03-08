b3487d18a912a89aa5490354b2d8c0bb33f1e92b
Damien Murphy <damien.murphy@deepgram.com>
2024-03-08 12:20:25 -0800

    add real time voice transcription

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,12 @@ import { useEffect, useRef, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { ModelIcon } from "@/components/icons/model-icon";
 import Link from "next/link";
+import { io } from 'socket.io-client';
 
+let connected = false;
+let socket;
 const DEFAULT_PROMPT =
-  "A cinematic shot of a baby raccoon wearing an intricate italian priest robe";
+  "A cat in an astronaut space suit";
 
 function randomSeed() {
   return Math.floor(Math.random() * 10000000).toFixed(0);
@@ -73,7 +76,79 @@ export default function Lightning() {
       prompt: prompt,
       seed: seed ? Number(seed) : Number(randomSeed()),
     });
+
+    if(!connected){
+      connected = true;
+      // Deepgram
+      socket = io('ws://localhost:3001', ({ transports: ["websocket"] }));
+    
+      socket.on("connect", async () => {
+        // connected = true;
+        console.log("client: connected to websocket");
+        await start(socket);
+      });
+    
+      socket.on("transcript", (transcript) => {
+        if (transcript !== "")
+          handleOnChange(transcript);
+          // captions.innerHTML = transcript ? `<span>${transcript}</span>` : "";
+      });
+    }
   }, []);
+
+  // Deeepgram
+  // const captions = window.document.getElementById("captions");
+
+  async function getMicrophone() {
+    const userMedia = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+    });
+  
+    return new MediaRecorder(userMedia);
+  }
+  
+  async function openMicrophone(microphone, socket) {
+    await microphone.start(500);
+  
+    microphone.onstart = () => {
+      console.log("client: microphone opened");
+      document.body.classList.add("recording");
+    };
+  
+    microphone.onstop = () => {
+      console.log("client: microphone closed");
+      document.body.classList.remove("recording");
+    };
+  
+    microphone.ondataavailable = (e) => {
+      console.log("client: sent data to websocket");
+      socket.emit("packet-sent", e.data);
+    };
+  }
+  
+  async function closeMicrophone(microphone) {
+    microphone.stop();
+  }
+  
+  async function start(socket) {
+    let microphone;
+  
+    console.log("client: waiting to open microphone");
+  
+    // listenButton.addEventListener("click", async () => {
+      if (!microphone) {
+        // open and close the microphone
+        microphone = await getMicrophone();
+        await openMicrophone(microphone, socket);
+      } else {
+        await closeMicrophone(microphone);
+        microphone = undefined;
+      }
+    // });
+  }
+
+
+  // Deepgram
 
   return (
     <main>
